14a7d8f5156ffb1a216e4319e366ee4889be8873
l.finke <l.finke@SCHULUNG22>
2023-01-13 15:25:47 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Taschenrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.strich = new System.Windows.Forms.Label();
+            this.ResultFullB = new System.Windows.Forms.Label();
+            this.SecondOben = new System.Windows.Forms.TextBox();
+            this.SecondUnten = new System.Windows.Forms.TextBox();
+            this.FirstUnten = new System.Windows.Forms.TextBox();
+            this.firstOben = new System.Windows.Forms.TextBox();
+            this.ResultObenB = new System.Windows.Forms.Label();
+            this.ResultUntenB = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.clearButton);
+            this.panel1.Controls.Add(this.divide);
+            this.panel1.Controls.Add(this.multiply);
+            this.panel1.Controls.Add(this.subtract);
+            this.panel1.Controls.Add(this.add);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.strich);
+            this.panel1.Controls.Add(this.ResultFullB);
+            this.panel1.Controls.Add(this.SecondOben);
+            this.panel1.Controls.Add(this.SecondUnten);
+            this.panel1.Controls.Add(this.FirstUnten);
+            this.panel1.Controls.Add(this.firstOben);
+            this.panel1.Controls.Add(this.ResultObenB);
+            this.panel1.Controls.Add(this.ResultUntenB);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(561, 375);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(197, 246);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 24;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearClick);
+            // 
+            // divide
+            // 
+            this.divide.AutoSize = true;
+            this.divide.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.divide.Location = new System.Drawing.Point(220, 202);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(21, 26);
+            this.divide.TabIndex = 23;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divideButton);
+            // 
+            // multiply
+            // 
+            this.multiply.AutoSize = true;
+            this.multiply.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.multiply.Location = new System.Drawing.Point(220, 170);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(22, 26);
+            this.multiply.TabIndex = 22;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiplicationButton);
+            // 
+            // subtract
+            // 
+            this.subtract.AutoSize = true;
+            this.subtract.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.subtract.Location = new System.Drawing.Point(220, 138);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(21, 26);
+            this.subtract.TabIndex = 21;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtractionButton);
+            // 
+            // add
+            // 
+            this.add.AutoSize = true;
+            this.add.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.add.Location = new System.Drawing.Point(217, 106);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(24, 26);
+            this.add.TabIndex = 20;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.additionButton);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(447, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 4);
+            this.label3.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(278, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 4);
+            this.label2.TabIndex = 18;
+            // 
+            // strich
+            // 
+            this.strich.BackColor = System.Drawing.SystemColors.Desktop;
+            this.strich.Location = new System.Drawing.Point(68, 170);
+            this.strich.Name = "strich";
+            this.strich.Size = new System.Drawing.Size(125, 4);
+            this.strich.TabIndex = 17;
+            // 
+            // ResultFullB
+            // 
+            this.ResultFullB.AutoSize = true;
+            this.ResultFullB.Location = new System.Drawing.Point(470, 277);
+            this.ResultFullB.Name = "ResultFullB";
+            this.ResultFullB.Size = new System.Drawing.Size(13, 16);
+            this.ResultFullB.TabIndex = 10;
+            this.ResultFullB.Text = "x";
+            
+            // 
+            // SecondOben
+            // 
+            this.SecondOben.Location = new System.Drawing.Point(294, 142);
+            this.SecondOben.Name = "SecondOben";
+            this.SecondOben.Size = new System.Drawing.Size(100, 22);
+            this.SecondOben.TabIndex = 9;
+            this.SecondOben.Text = "2";
+            this.SecondOben.TextChanged += new System.EventHandler(this.TextChangeor);
+            // 
+            // SecondUnten
+            // 
+            this.SecondUnten.Location = new System.Drawing.Point(294, 188);
+            this.SecondUnten.Name = "SecondUnten";
+            this.SecondUnten.Size = new System.Drawing.Size(100, 22);
+            this.SecondUnten.TabIndex = 8;
+            this.SecondUnten.Text = "-3";
+            this.SecondUnten.TextChanged += new System.EventHandler(this.TextChangeUr);
+            // 
+            // FirstUnten
+            // 
+            this.FirstUnten.Location = new System.Drawing.Point(81, 188);
+            this.FirstUnten.Name = "FirstUnten";
+            this.FirstUnten.Size = new System.Drawing.Size(100, 22);
+            this.FirstUnten.TabIndex = 7;
+            this.FirstUnten.Text = "7";
+            this.FirstUnten.TextChanged += new System.EventHandler(this.TextChangeul);
+            // 
+            // firstOben
+            // 
+            this.firstOben.Location = new System.Drawing.Point(81, 142);
+            this.firstOben.Name = "firstOben";
+            this.firstOben.Size = new System.Drawing.Size(100, 22);
+            this.firstOben.TabIndex = 6;
+            this.firstOben.Text = "-3";
+            this.firstOben.TextChanged += new System.EventHandler(this.TextChangeol);
+            // 
+            // ResultObenB
+            // 
+            this.ResultObenB.AutoSize = true;
+            this.ResultObenB.Location = new System.Drawing.Point(470, 138);
+            this.ResultObenB.Name = "ResultObenB";
+            this.ResultObenB.Size = new System.Drawing.Size(13, 16);
+            this.ResultObenB.TabIndex = 5;
+            this.ResultObenB.Text = "x";
+            // 
+            // ResultUntenB
+            // 
+            this.ResultUntenB.AutoSize = true;
+            this.ResultUntenB.Location = new System.Drawing.Point(470, 194);
+            this.ResultUntenB.Name = "ResultUntenB";
+            this.ResultUntenB.Size = new System.Drawing.Size(13, 16);
+            this.ResultUntenB.TabIndex = 4;
+            this.ResultUntenB.Text = "x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 375);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ResultObenB;
+        private System.Windows.Forms.Label ResultUntenB;
+        private System.Windows.Forms.TextBox SecondOben;
+        private System.Windows.Forms.TextBox SecondUnten;
+        private System.Windows.Forms.TextBox FirstUnten;
+        private System.Windows.Forms.TextBox firstOben;
+        private System.Windows.Forms.Label ResultFullB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label strich;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Taschenrechner/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Taschenrechner
+{
+
+
+    // als nächstes Kürzen, das ergebnis vorallem mit GGT
+    // 
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // these integers declare the numbers we are calculating with
+
+        //Fs = first Sn = second Br = Bruch fl = full ob = oben un = unten
+        // fl = ob/un
+
+        double FirstBruchFull;
+        double FirstBruchOben;
+        double FirstBruchUnten;
+
+        double SecondBruchFull;
+        double SecondBruchOben;
+        double SecondBruchUnten;
+
+        double ResultFull;
+        double ResultOben;
+        double ResultUnten;
+
+        double ZählerLinks;
+        double ZählerRechts;
+        double ZählerGekürztLinks;
+        double ZählerGekürztRechts;
+        double NennerGekürztLinks;
+        double NennerGekürztRechts;
+
+
+
+        bool TextChangedol = false;
+        bool TextChangedul = false;
+        bool TextChangedor = false;
+        bool TextChangedur = false;
+
+        bool addWasClicked = false;
+        bool subtractWasClicked = false;
+        bool multiplyWasClicked = false;
+        bool divideWasClicked = false;
+
+
+         
+
+         
+
+
+
+        private void StartButton_Click_1(object sender, EventArgs e)
+        {
+            StartTheCalculator();
+
+        }
+
+        public void StartTheCalculator()
+        {
+            if (TextChangedur == true && TextChangedul == true &&
+            TextChangedor == true && TextChangedol == true)
+            {
+                Rechner();
+
+            }
+            else MessageBox.Show("Please input all values!");
+            
+
+        }
+
+        public void Rechner()
+        {
+            
+            // first make the values readable to allow the calculator to do what it needs to do
+            FirstBruchOben = double.Parse(firstOben.Text);
+            FirstBruchUnten = double.Parse(FirstUnten.Text);
+            SecondBruchOben = double.Parse(SecondOben.Text);
+            SecondBruchUnten = double.Parse(SecondUnten.Text);
+
+            // manual check if results were accurate
+                FirstBruchFull = FirstBruchOben / FirstBruchUnten;
+                SecondBruchFull = SecondBruchOben / SecondBruchUnten;
+
+            // multiplizieren
+            if(multiplyWasClicked == true)
+            {
+                // manual check to be deleted
+                ResultFull = FirstBruchFull * SecondBruchFull;
+                ResultFullB.Text = ResultFull.ToString();
+
+                ResultOben = FirstBruchOben * SecondBruchOben;
+                ResultObenB.Text = ResultOben.ToString();
+                ResultUnten = FirstBruchUnten * SecondBruchUnten;
+                ResultUntenB.Text = ResultUnten.ToString();
+            }
+
+            // dividieren
+            if (divideWasClicked == true)
+            {
+                // manual check to be deleted
+                ResultFull = FirstBruchFull / SecondBruchFull;
+                ResultFullB.Text = ResultFull.ToString();
+
+                ResultOben = FirstBruchOben * SecondBruchUnten;
+                ResultObenB.Text = ResultOben.ToString();
+                ResultUnten = FirstBruchUnten * SecondBruchOben;
+                ResultUntenB.Text = ResultUnten.ToString();
+            }
+
+            // addieren
+            if(addWasClicked == true)
+            {
+                // manual check to be deleted
+                ResultFull = FirstBruchFull + SecondBruchFull;
+                ResultFullB.Text = ResultFull.ToString();
+
+                // var kGV = Getsor(FirstBruchUnten, SecondBruchUnten)
+
+                // var multiplikatorZaehlerLinks = kGV / zaehlerLinks;
+                // var mulitlikatorZaehlerrechts = kGV / ZaehlerRechts;
+                // var zaehlerLinks = Zahlerlinks*multiplikatorZaehlerLinks;
+                // var zahlerrechts = zahlerrechts * mulitlikatorZaehlerrechts;
+                //  var ergebnissZehaler = zaehlerLinks + zahlerrechts;
+                // nennerErgeebniss = kGV;
+
+                /*
+                 *  X_1   - Zähler
+                 *  ____
+                 *  Y_1  - Nenner
+                 */
+
+
+
+                var kGV = Getsor(FirstBruchUnten, SecondBruchUnten);
+
+                ZählerGekürztLinks = FirstBruchOben;
+                ZählerGekürztRechts = SecondBruchOben;
+                NennerGekürztLinks = FirstBruchUnten;
+                NennerGekürztRechts = SecondBruchUnten;
+
+                
+
+                var multiplikatorObenLinks = kGV / FirstBruchUnten;
+                var multiplikatorObenRechts = kGV / SecondBruchUnten;
+                ZählerLinks = FirstBruchOben * multiplikatorObenLinks;
+                ZählerRechts = SecondBruchOben * multiplikatorObenRechts;
+                ResultOben = ZählerLinks + ZählerRechts;
+                
+                ResultUnten = kGV;
+                
+
+
+                if(ResultOben == 0)
+                {
+                    ResultUnten = 0;
+                }
+                ResultObenB.Text = ResultOben.ToString();
+                ResultUntenB.Text = ResultUnten.ToString();
+            }
+
+            if (subtractWasClicked == true)
+            {
+                // manual check to be deleted
+                ResultFull = FirstBruchFull - SecondBruchFull;
+                ResultFullB.Text = ResultFull.ToString();
+
+                
+
+                var kGV = Getsor(FirstBruchUnten, SecondBruchUnten);
+                var multiplikatorObenLinks = kGV / FirstBruchUnten;
+                var multiplikatorObenRechts = kGV / SecondBruchUnten;
+                ZählerLinks = FirstBruchOben * multiplikatorObenLinks;
+                ZählerRechts = SecondBruchOben * multiplikatorObenRechts;
+                
+                ResultOben = ZählerLinks - ZählerRechts;
+                
+                ResultUnten = kGV;
+
+                if (ResultOben == 0)
+                {
+                    ResultUnten = 0;
+                }
+                ResultObenB.Text = ResultOben.ToString();
+                ResultUntenB.Text = ResultUnten.ToString();
+
+
+            }
+
+        }
+
+        private double Getsor(double nennerL, double nennerR) //Beispiel 2 und 3
+        {
+            // 
+            double num1, num2;
+
+            if (nennerL > nennerR)
+            {
+                num1 = nennerL; num2 = nennerR;
+            }
+            else
+            {
+                num1 = nennerR; num2 = nennerL;
+            }
+            // nummer 1 ist immer größer als nummer 2
+            // und ist immer der größere von den beiden Nennern
+
+            for (double i = 1; i < num2; i++) // von 1 bis kleinste Nenner
+            {
+                double mult = num1 * i;
+                if (mult % num2 == 0)
+                {
+                    return mult;
+                }
+            }
+
+            return num1 * num2;
+
+        }
+
+        //private static ulong GCD(ulong a, ulong b)
+        //{
+        //    while (a != 0 && b != 0) // solange a != 0 und b != 0 
+        //    {
+        //        if (a > b)
+        //            a = a % b;
+        //        else
+        //            b = b % a;
+        //    }
+
+        //    return a | b;
+        //}
+
+
+
+        /**public static string ToFractions(double number, int precision = 4)
+        {
+            int w, n, d;
+            RoundToMixedFraction(number, precision, out w, out n, out d);
+            var ret = $"{w}";
+            if (w > 0)
+            {
+                if (n > 0)
+                    ret = $"{w} {n}/{d}";
+            }
+            else
+            {
+                if (n > 0)
+                    ret = $"{n}/{d}";
+            }
+            return ret;
+        }
+
+        static void RoundToMixedFraction(double input, int accuracy, out int whole, out int numerator, out int denominator)
+        {
+            double dblAccuracy = (double)accuracy;
+            whole = (int)(Math.Truncate(input));
+            var fraction = Math.Abs(input - whole);
+            if (fraction == 0)
+            {
+                numerator = 0;
+                denominator = 1;
+                return;
+            }
+            var n = Enumerable.Range(0, accuracy + 1).SkipWhile(e => (e / dblAccuracy) < fraction).First();
+            var hi = n / dblAccuracy;
+            var lo = (n - 1) / dblAccuracy;
+            if ((fraction - lo) < (hi - fraction)) n--;
+            if (n == accuracy)
+            {
+                whole++;
+                numerator = 0;
+                denominator = 1;
+                return;
+            }
+            var gcd = GCD(n, accuracy);
+            numerator = n / gcd;
+            denominator = accuracy / gcd;
+        }
+
+        static int GCD(int a, int b)
+        {
+            if (b == 0) return a;
+            else return GCD(b, a % b);
+        }**/
+
+
+
+
+        private void TextChangeol(object sender, EventArgs e)
+        {
+            TextChangedol = true;
+        }
+
+        private void TextChangeul(object sender, EventArgs e)
+        {
+            
+            TextChangedul = true;
+            
+        }
+
+        private void TextChangeor(object sender, EventArgs e)
+        {
+            TextChangedor = true;
+            
+        }
+
+        private void TextChangeUr(object sender, EventArgs e)
+        {
+            TextChangedur = true;
+        }
+
+       
+
+        private void ResultFull_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void additionButton(object sender, EventArgs e)
+        {
+             addWasClicked = true;
+            StartTheCalculator();
+            clearRechenart();
+
+            return;
+            
+        }
+
+        private void subtractionButton(object sender, EventArgs e)
+        {
+             subtractWasClicked = true;
+            StartTheCalculator();
+            clearRechenart();
+
+            return;
+        }
+
+        private void multiplicationButton(object sender, EventArgs e)
+        {
+             multiplyWasClicked = true;
+            StartTheCalculator();
+            clearRechenart();
+
+            return;
+
+        }
+
+        private void divideButton(object sender, EventArgs e)
+        {
+             divideWasClicked = true;
+            StartTheCalculator();
+            clearRechenart();
+            return;
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+            FirstBruchOben = 0;
+            firstOben.Text = FirstBruchOben.ToString();
+            FirstBruchUnten = 0;
+            FirstUnten.Text = FirstBruchUnten.ToString();
+            SecondBruchOben = 0;
+            SecondOben.Text = SecondBruchOben.ToString();
+            SecondBruchUnten = 0;
+            SecondUnten.Text = SecondBruchUnten.ToString();
+            ResultOben = 0;
+            ResultObenB.Text = ResultOben.ToString();
+            ResultUnten = 0;
+            ResultUntenB.Text = ResultUnten.ToString();
+            ResultFull = 0;
+            ResultFullB.Text = ResultFull.ToString();
+
+
+        }
+
+        private void clearRechenart()
+        {
+            addWasClicked = false;
+            subtractWasClicked = false;
+            multiplyWasClicked = false;
+            divideWasClicked = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
